88d92fe130ec458d423c27d165558ca9cd2d8bf5
Aruna Parameswaran <aruna.parame@gmail.com>
2023-05-02 19:13:56 -0500

    Added ability for user to upload a transcript of their choice

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 import utils
 
@@ -9,6 +9,14 @@ def home():
     transcript = utils.get_transcript()
     result = utils.get_response_from_chatgpt(utils.construct_prompt(transcript=transcript))
     return render_template('index.html', result=result, transcript=transcript)
+
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        file_data = f.read().decode('utf-8')
+        result = utils.get_response_from_chatgpt(utils.construct_prompt(transcript=file_data))
+        return render_template('index.html', result=result, transcript=file_data)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
